0e59378d1501abbd295ab0d61f407136ee908645
Steven <steven001@gmail.com>
2023-08-18 17:01:42 +1000

    feat: enable onboarding and auto-discovery

--- config.js
+++ config.js
@@ -4,8 +4,8 @@ module.exports = {
   endpoint: "https://git.sbbh.cloud/api/v1/",
   gitAuthor: "Renovate Bot <renovate@sbbh.cloud>",
   timezone: "Australia/Melbourne",
-  autodiscover: false,
-  onboarding: false,
+  autodiscover: true,
+  onboarding: true,
   extends: [
     "local>sysadm/renovate-config:base",
     "local>sysadm/renovate-config:repositories",
